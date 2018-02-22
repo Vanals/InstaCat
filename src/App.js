d29cf5d8aabf7eb65da97203b7d473741950151a
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-22 14:29:18 +0000

    Save photoCardsData into App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+const photoCardsData = require('./photoCardsData.json');
 // import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoCards: photoCardsData
+    };
+  }
+
+
   render() {
     return (
       <div>
