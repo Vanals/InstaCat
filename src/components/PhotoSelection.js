decc82306b3890f9edf787f19e39fa10584bf6dc
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-23 15:29:47 +0000

    Add p tag which shows the description of the photo selected. Given to the p tag that shows the user name an id with value 'username'.  Tests need to be fixed

--- src/components/PhotoSelection.js
+++ src/components/PhotoSelection.js
@@ -7,8 +7,9 @@ class PhotoSelection extends Component {
     return (
       <div>
         <div> <img id='avatar' src={avatarPath} alt='avatarPicture'/>  </div>
-        <p>{this.props.photoData.username}</p>
+        <p id='username'>{this.props.photoData.username}</p>
         <div> <img id='photo-selected' src={imgPath} alt='pictureSelected' /> </div>
+        <p id='description-photo'>{this.props.photoData.description}</p>
       </div>
     );
   }
