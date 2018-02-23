249b3de58c8c3e198cce346b198a713f80578e89
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-23 12:55:04 +0000

    AddPhotoSelection component in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PhotosFeed from './components/PhotosFeed'
+import PhotoSelection from './components/PhotoSelection'
 const photoCardsData = require('./photoCardsData.json');
 // import './App.css';
 
@@ -7,16 +8,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photoCards: photoCardsData
+      photoCards: photoCardsData,
+      selectedPhoto: []
     };
   }
 
 
+  photoSelectionHandler = (index) => {
+    this.setState({selectedPhoto: this.state.photoCards[index] })
+  }
+
   render() {
     return (
+    <div>
       <div>
-        <PhotosFeed photoCards={this.state.photoCards}/>
+        <PhotosFeed photoCards={this.state.photoCards} photoSelectionHandler={this.photoSelectionHandler}/>
       </div>
+      <div>
+        <PhotoSelection photoName={this.state.selectedPhoto.imageLink}/>
+      </div>
+    </div>
     );
   }
 }
