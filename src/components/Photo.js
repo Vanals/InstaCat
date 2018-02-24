396eb89326d99c5bb4ebcbbc164dd421f07447fc
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-24 14:08:25 +0000

    Add alt attribute to img tag in Photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,9 +4,10 @@ import '../styles/Photo.css'
 class Photo extends Component {
   render() {
     const imgPath = "./images/" + `${this.props.photoName}`
+    const altText = "" + `${this.props.photoName}} not loaded`
     return (
       <div>
-        <div> <img src={imgPath} className='cat-photo' onClick={() => this.props.photoSelectionHandler(this.props.index)}/> </div>
+        <div> <img src={imgPath} className='cat-photo' onClick={() => this.props.photoSelectionHandler(this.props.index)} alt={altText} /> </div>
       </div>
     );
   }
