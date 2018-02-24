a113336339bf4d32d1c9ef35559cef5b66953457
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-24 11:13:37 +0000

    Fix likeButtonHandlertest + create setIndexKey function test

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@ describe('App Component', () => {
     wrapper = shallow(<App />);
 
     wrapperInstance = wrapper.instance()
-    wrapperInstance.setState({photoCards: [ {likes:53} ] })
+    wrapperInstance.setState({photoCards: [ {likes:53}, {likes:100} ], selectedPhoto: { indexKey: 0} })
   });
 
   it('renders without crashing', () => {
@@ -27,8 +27,14 @@ describe('App Component', () => {
   });
 
   it('call likeButtonHandler, which increase the like amount of the selected picture by 1', () => {
-    expect(wrapperInstance.likeButtonHandler(0))
+    expect(wrapperInstance.likeButtonHandler())
     expect(wrapperInstance.state.photoCards[0].likes).toEqual(54);
+  });
+
+  it('call setIndexKey', () => {
+    expect(wrapperInstance.setIndexKey())
+    expect(wrapperInstance.state.photoCards[0].indexKey).toEqual(0);
+    expect(wrapperInstance.state.photoCards[1].indexKey).toEqual(1);
   });
 
 })
