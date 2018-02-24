c625b56b673503c331bf409fda0234862f1a515f
Marco Vanali <Marco91Vanali@gmail.com>
2018-02-24 14:20:06 +0000

    Add test for hide button

--- src/components/__tests__/Photo-tests.js
+++ src/components/__tests__/Photo-tests.js
@@ -11,7 +11,10 @@ describe('Photo Component', () => {
   });
 
   it('render the img tag', () => {
-    expect(wrapper.find('.cat-photo').length).toEqual(1)
+    expect(wrapper.find('#cat-photo').length).toEqual(1)
   })
 
+  it('render the hide button', () => {
+    expect(wrapper.find('#hide-button').length).toEqual(1)
+  })
 })
