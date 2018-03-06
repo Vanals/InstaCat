1eeef1b7164c7b5e33868d0a06a71654298c0e7c
Marco Vanali <Marco91Vanali@gmail.com>
2018-03-06 16:47:20 +0000

    Create seed for database, copying the photoCardsData.json file +  Set up server
    The server after seed, is able to show the photoCards information.

--- back-end/seed.js
+++ back-end/seed.js
@@ -15,9 +15,59 @@ mongoose.connection.on('open', function() {
       liked: true
     });
 
-  console.log(cat1.imageLink);
+    cat1.save(function (err, fluffy) {
+      if (err) return console.error(err);
+    });
 
-  cat1.save(function (err, fluffy) {
+  const cat2 = new PhotoCard({
+    username: "kaedenor",
+    userIconLink: "kaedenor1.jpg",
+    imageLink: "cat2.jpeg",
+    description: "It is a very inconvenient habit of kittens (Alice had once made the remark) that, whatever you say to them they always purr.",
+    likes: 673,
+    liked: false
+  });
+
+  cat2.save(function (err, fluffy) {
+    if (err) return console.error(err);
+  });
+
+  const cat3 = new PhotoCard({
+    username: "damarionei",
+    userIconLink: "damarionei2.jpg",
+    imageLink: "cat3.jpeg",
+    description: "Way down deep, we’re all motivated by the same urges. Cats have the courage to live by them.",
+    likes: 292,
+    liked: false
+  });
+
+  cat3.save(function (err, fluffy) {
+    if (err) return console.error(err);
+  });
+
+  const cat4 = new PhotoCard({
+    username: "ricpena",
+    userIconLink: "ricpena1.jpg",
+    imageLink: "cat4.jpeg",
+    description: "A cat has absolute emotional honesty: human beings, for one reason or another, may hide their feelings, but a cat does not.",
+    likes: 785,
+    liked: true
+  });
+
+  cat4.save(function (err, fluffy) {
+    if (err) return console.error(err);
+  });
+
+  const cat5 = new PhotoCard({
+    username: "zekebates",
+    userIconLink: "zekebates.jpg",
+    imageLink: "cat5.jpeg",
+    description: "If a dog jumps into your lap it is because he is fond of you; but if a cat does the same thing it is because your lap is warmer.",
+    likes: 497,
+    liked: false
+  });
+
+  cat5.save(function (err, fluffy) {
     if (err) return console.error(err);
   });
 
---
+++ back-end/server.js
@@ -0,0 +1,77 @@
+// call the packages we need
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const photoCard = require('./models/photoCard');
+const db = require('./db');
+const PhotoCard = require('./models/photoCard');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set our port
+
+var port = process.env.PORT || 8080;
+
+// get an instance of the express Router
+
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+
+router.route('/photoCard')
+  // create a bear (accessed at POST http://localhost:8080/api/bears)
+  .get(function(req, res) {
+    PhotoCard.find(function(err, photocards) {
+      if (err)
+      res.send(err);
+
+      res.json(photocards);
+    });
+  });
+
+//THE FOLLOWING CODE IS  FOR A POST REQUEST, BUT FOR THE MOMENT I DON'T WANT TO ENABLE IT
+
+// router.route('/photoCard')
+//
+//   .post(function(req, res) {
+//
+//     var photocard = new PhotoCard();      // create a new instance of the Bear model
+//     photocard.name = req.body.name;  // set the bears name (comes from the request)
+//
+//     // save the bear and check for errors
+//     photocard.save(function(err) {
+//         if (err)
+//             res.send(err);
+//
+//         res.json({ message: 'PhotoCard created!' });
+//     });
+//
+//   });
+
+  // get all the photocards (accessed at GET http://localhost:8080/api/photoCard)
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
