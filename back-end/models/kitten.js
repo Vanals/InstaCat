41a7f70b2da77d67d69c049b2209e421dfbd1549
Marco Vanali <Marco91Vanali@gmail.com>
2018-03-06 11:18:50 +0000

    Delete not useful methods, just used for learning mongoose

--- back-end/models/kitten.js
+++ back-end/models/kitten.js
@@ -10,13 +10,6 @@ let kittySchema = mongoose.Schema({
   liked: Boolean
 });
 
-kittySchema.methods.speak = function () {
-  var greeting = this.name
-    ? "Meow name is " + this.name
-    : "I don't have a name";
-  console.log(greeting);
-}
-
 let Kitten = mongoose.model('Kitten', kittySchema);
 
 module.exports = Kitten;
